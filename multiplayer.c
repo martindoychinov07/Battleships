3efdc9ab52a883522216ac4311bb4a1bb10a88b7
martindoychinov07 <114239959+martindoychinov07@users.noreply.github.com>
2024-06-14 19:33:39 +0300

    Add files via upload

--- multiplayer.c
+++ multiplayer.c
@@ -26,6 +26,15 @@ typedef struct {
     char own_display[BOARD_SIZE][BOARD_SIZE]; 
 } Player;
 
+struct ShipArr {
+    int size;
+    int count;
+};
+
+typedef struct ShipArr ShipArr;
+
+ShipArr ships[] = {{6, 1}, {4, 2}, {3, 3}, {2, 4}};
+
 void initialize_board(Player *player);
 void print_board(char board[BOARD_SIZE][BOARD_SIZE]);
 bool place_ship(Player *player, int size, char orientation, Coordinate start);
@@ -40,37 +49,170 @@ bool player_turn(Player *current, Player *opponent);
 void edit_ship(Player *player);
 void view_board(Player *player, bool during_configuration);
 bool all_ships_destroyed(Player *player);
+void botAttack(char playerBoard[BOARD_SIZE][BOARD_SIZE]);
+int validPos(char board[BOARD_SIZE][BOARD_SIZE], int x, int y);
+int validArea(char board[BOARD_SIZE][BOARD_SIZE], int x, int y, int size, char dir);
+void randomShips(char board[BOARD_SIZE][BOARD_SIZE]);
 
 int main() {
-    Player player1, player2;
-    initialize_board(&player1);
-    initialize_board(&player2);
+    char mode;
 
-    char choice;
-    printf("Player 1: Do you want to load a configuration from a file? (y/n): ");
-    scanf(" %c", &choice);
-    if (choice == 'y') {
-        char filename[100];
-        printf("Enter filename: ");
-        scanf("%s", filename);
-        load_configuration(&player1, filename);
-    } else {
-        manual_configuration(&player1);
+    printf("To play multiplayer press(m) and to play singleplayer press(s):");
+    scanf("%c", &mode);
+
+    while(mode != 'm' && mode !='s') {
+        printf("Wrong choice! Please try again!");
+        printf("To play multiplayer press(m) and to play singleplayer press(s):");
+        scanf("%c", &mode);
     }
 
-    printf("Player 2: Do you want to load a configuration from a file? (y/n): ");
-    scanf(" %c", &choice);
-    if (choice == 'y') {
-        char filename[100];
-        printf("Enter filename: ");
-        scanf("%s", filename);
-        load_configuration(&player2, filename);
-    } else {
-        manual_configuration(&player2);
+    if(mode == 'm') {
+        Player player1, player2;
+        initialize_board(&player1);
+        initialize_board(&player2);
+
+        char choice;
+        printf("Player 1: Do you want to load a configuration from a file? (y/n): ");
+        scanf(" %c", &choice);
+        if (choice == 'y') {
+            char filename[100];
+            printf("Enter filename: ");
+            scanf("%s", filename);
+            load_configuration(&player1, filename);
+        } else {
+            manual_configuration(&player1);
+        }
+
+        printf("Player 2: Do you want to load a configuration from a file? (y/n): ");
+        scanf(" %c", &choice);
+        if (choice == 'y') {
+            char filename[100];
+            printf("Enter filename: ");
+            scanf("%s", filename);
+            load_configuration(&player2, filename);
+        } else {
+            manual_configuration(&player2);
+        }
+
+        play_game(&player1, &player2);
+    }
+    else if(mode == 's') {
+        Player player, bot;
+
+        initialize_board(&player);
+        initialize_board(&bot);
+
+        char choice;
+        printf("Player 1: Do you want to load a configuration from a file? (y/n): ");
+        scanf(" %c", &choice);
+        if (choice == 'y') {
+            char filename[100];
+            printf("Enter filename: ");
+            scanf("%s", filename);
+            load_configuration(&player, filename);
+        } else {
+            manual_configuration(&player);
+        }
+
+        printf("Player 2: Do you want to load a configuration from a file? (y/n): ");
+        scanf(" %c", &choice);
+        if (choice == 'y') {
+            char filename[100];
+            printf("Enter filename: ");
+            scanf("%s", filename);
+            load_configuration(&bot, filename);
+        } else {
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            // randomShips(bot.);
+        }
+
+        play_game(&player, &player);
     }
 
-    play_game(&player1, &player2);
     return 0;
+}
+
+void botAttack(char playerBoard[BOARD_SIZE][BOARD_SIZE]) {
+    int x = rand() % BOARD_SIZE;
+    int y = rand() % BOARD_SIZE;
+
+    while (playerBoard[y][x] == 'H' || playerBoard[y][x] == 'M') {
+        x = rand() % BOARD_SIZE;
+        y = rand() % BOARD_SIZE;
+    }
+
+    if (playerBoard[y][x] == '-') {
+        playerBoard[y][x] = 'H';
+    }
+}
+
+int validPos(char board[BOARD_SIZE][BOARD_SIZE], int x, int y) {
+    if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) {
+        return 0;
+    }
+    else {
+        for (int xi = -1; xi <= 1; xi++) {
+            for (int yi = -1; yi <= 1; yi++) {
+                int tx = x + xi;
+                int ty = y + yi;
+                if (0 <= tx && tx < BOARD_SIZE && 0 <= ty && ty < BOARD_SIZE) {
+                    if (board[ty][tx] != ' ') {
+                        return 0;
+                    }
+                }
+            }
+        }
+        return 1;
+    }
+}
+
+int validArea(char board[BOARD_SIZE][BOARD_SIZE], int x, int y, int size, char dir) {
+    for (int i = 0; i < size; i++) {
+        int valid = validPos(board, x + (dir == 'L' ? -i: dir == 'R' ? i : 0), y + (dir == 'U' ? -i: dir == 'D' ? i : 0));
+        if (valid == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void randomShips(char board[BOARD_SIZE][BOARD_SIZE]) {
+    int count = (sizeof(ships) / sizeof(ships[0]));
+
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j < ships[i].count; j++) {
+            int x = rand() % BOARD_SIZE;
+            int y = rand() % BOARD_SIZE;
+            char dir = "UDLR"[rand() % 4];
+
+            while (validArea(board, x, y, ships[i].size, dir) == 0) {
+                x = rand() % BOARD_SIZE;
+                y = rand() % BOARD_SIZE;
+                dir = "UDLR"[rand() % 4];
+            }
+
+            for (int m = 0; m < ships[i].size; m++) {
+                if (dir == 'U') {
+                    board[y - m][x] = '|';
+                }
+                else if (dir == 'D') {
+                    board[y + m][x] = '|';
+                }
+                else if (dir == 'L') {
+                    board[y][x - m] = '-';
+                }
+                else if (dir == 'R') {
+                    board[y][x + m] = '-';
+                }
+            }
+        }
+    }
 }
 
 void initialize_board(Player *player) {
